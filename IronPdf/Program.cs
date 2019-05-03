c0ab946ea2a79281ee048bc9f8aa09f09bfa4cdc
Chris Wright <chriswright@borderexpress.com.au>
2019-05-03 20:37:04 +1000

    Add project files.

---
+++ IronPdf/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace IronPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var populatedTemplate = "<html><head></head><body>hello</body></html>";
+
+            var pdfRenderer = new IronPdf.HtmlToPdf();
+       
+            var pdf = pdfRenderer.RenderHtmlAsPdf(populatedTemplate);
+            
+            var file = pdf.BinaryData;
+
+            Console.WriteLine(file.Length.ToString());
+            
+            Console.ReadLine();
+        }
+
+      
+    }
+}
